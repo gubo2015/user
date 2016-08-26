0c34000890bfd19b52de95852855c3527b002a56
wujunshan <wujun-shan@qq.com>
2016-08-26 17:08:41 +0800

    add User

---
+++ app/models/user.rb
@@ -0,0 +1,14 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  password   :string
+#  address    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class User < ApplicationRecord
+end
